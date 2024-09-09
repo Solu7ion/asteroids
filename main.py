7001ec07d83e01c7d37ea7a1a3f22e2a875b5145
Solu7ion <armanid03.02@mail.ru>
2024-09-09 18:16:50 +0500

    Add close functionality and window(black)

--- main.py
+++ main.py
@@ -2,9 +2,22 @@ import pygame
 from constants import *
 
 def main():
-    print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_HEIGHT, SCREEN_HEIGHT))
 
+ 
+    black = (0, 0, 0)
+
+    
+    running = True
+    while running:
+        screen.fill(black)
+        
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+
+        
 if __name__ == "__main__":
     main()
