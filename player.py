75d3e411af998fe0ada1faa0f1792d4b4d780f81
Solu7ion <armanid03.02@mail.ru>
2024-09-10 13:36:08 +0500

    Fix interval shoots

--- player.py
+++ player.py
@@ -7,7 +7,7 @@ class Player(CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)  
         self.rotation = 0  
-
+        self.shoot_timer = 0
         # in the player class
     def triangle(self):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
@@ -24,6 +24,7 @@ class Player(CircleShape):
        self.rotation += PLAYER_TURN_SPEED * dt
         
     def update(self, dt):
+        self.shoot_timer -=dt
         keys = pygame.key.get_pressed()
 
         if keys[pygame.K_w]:
@@ -46,6 +47,10 @@ class Player(CircleShape):
         self.position += forward * PLAYER_SPEED * dt
 
     def shoot(self):
+        if self.shoot_timer > 0:
+            return
+        self.shoot_timer = PLAYER_SHOOT_COOLDOWN
         shot = Shot(self.position.x, self.position.y)
         shot.velocity = pygame.Vector2(0, 1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
 
+    
