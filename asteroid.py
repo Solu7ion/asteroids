82132c9d01d8bffeb5549b43552cedd3f240679c
Solu7ion <armanid03.02@mail.ru>
2024-09-10 13:54:46 +0500

    Add split() asteroid

--- asteroid.py
+++ asteroid.py
@@ -1,6 +1,7 @@
 import pygame 
 import random
 
+
 from circleshape import *
 from constants import *
 class Asteroid(CircleShape):
@@ -14,3 +15,20 @@ class Asteroid(CircleShape):
     def update(self, dt):
         # Update the position based on velocity and time delta
         self.position += self.velocity * dt
+    
+    def split(self):
+        self.kill()
+       
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            return 
+        
+        random_angle = random.uniform(20, 50)
+
+        a = self.velocity.rotate(random_angle)
+        b = self.velocity.rotate(-random_angle)
+
+        new_radius = self.radius - ASTEROID_MIN_RADIUS
+        asteroid = Asteroid(self.position.x, self.position.y, new_radius)
+        asteroid.velocity = a * 1.2
+        asteroid = Asteroid(self.position.x, self.position.y, new_radius)
+        asteroid.velocity = b * 1.2
--- main.py
+++ main.py
@@ -46,8 +46,8 @@ def main():
             for shot in shots:
                 if asteroid.collides_with(shot):
                     shot.kill()
-                    asteroid.kill()
-                
+                    asteroid.split()
+                    
         screen.fill("black")
         for obj in drawable:
             obj.draw(screen)
